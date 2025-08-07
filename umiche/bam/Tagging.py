aa8bdb99d0fb74dce8e597d17b07754ab1edcac3
Jianfeng Sun <jianfeng.sunmt@gmail.com>
2025-08-07 20:18:33 +0100

    tagging for general
    tagging for general

--- umiche/bam/Tagging.py
+++ umiche/bam/Tagging.py
@@ -5,6 +5,7 @@ __developer__ = "Jianfeng Sun"
 __maintainer__ = "Jianfeng Sun"
 __email__="jianfeng.sunmt@gmail.com"
 
+from typing import Dict
 import re
 
 from umiche.util.Console import Console
@@ -34,19 +35,21 @@ class Tagging:
     def add_from_header(
             self,
             delimiter=r'[_:]',
-            bc_idx: int = -2,
-            umi_idx: int = -1,
-            bc_tag_name: str = 'CB',
-            umi_tag_name: str = 'MB',
+            tag_info: Dict={
+                'CB' : {'idx': -2, 'val_type': 'Z'},
+                'MB': {'idx': -1, 'val_type': 'Z'},
+            },
             strip_after: bool = False,
     ) -> str:
         with self.pysam.AlignmentFile(self.bam_fpn, "rb") as bam_in, \
                 self.pysam.AlignmentFile(self.sv_bam_fpn, "wb", template=bam_in) as bam_out:
 
+            tag_running_title = ["&".join(i) for i in tag_info.keys()]
+            tag_indices = [item['idx'] for item in tag_info.values()]
             for read in self.console._tqdm(
                     bam_in,
                     # total=bam_in.count(until_eof=True),
-                    desc=f"[{bc_tag_name} & {umi_tag_name} tagging]",
+                    desc=f"[{tag_running_title} tagging]",
                     unit="reads",
                     position=0,
                     leave=True,
@@ -54,18 +57,15 @@ class Tagging:
             ):
                 parts = re.split(delimiter, read.query_name)
                 # print(parts)
-                if len(parts) <= max(bc_idx, umi_idx):
+                if len(parts) <= max(tag_indices):
                     # 当 read name is not qualified
                     bam_out.write(read)
                     continue
 
-                bc = parts[bc_idx]
-                umi = parts[umi_idx]
-                # print(bc)
-                # print(umi)
-                print(read.get_tag("XF"))
-                read.set_tag(bc_tag_name, bc, value_type="Z")
-                read.set_tag(umi_tag_name, umi, value_type="Z")
+                for tag_name, tag_val in tag_info.items():
+                    target = parts[tag_val['idx']]
+                    # print(read.get_tag("XF"))
+                    read.set_tag(tag_name, target, value_type=tag_val['val_type'])
                 if strip_after:
                     read.query_name = parts[0]
 
@@ -75,12 +75,21 @@ class Tagging:
 
 
 if __name__ == "__main__":
-    from umiche.path import to
-    #  samtools view /mnt/d/Document/Programming/Python/umiche/umiche/data/r1/trumicount/10xn9k_10c/10xn9k_10c_tagged.bam | head -n 5
+    # from umiche.path import to
+    # samtools view /mnt/d/Document/Programming/Python/umiche/umiche/data/r1/trumicount/10xn9k_10c/10xn9k_10c_tagged.bam | head -n 5
     # python Trumicount.py --input-bam /mnt/d/Document/Programming/Python/umiche/umiche/data/r1/trumicount/10xn9k_10c/10xn9k_10c_tagged.sorted.bam --cell-tag CB --umi-tag MB --gene-tag XF --threshold 2 --output-counts sample.counts.tsv --output-assigned sample.assigned.tsv
     p = Tagging(
-        bam_fpn=to('data/r1/trumicount/10xn9k_10c/10xn9k_10c.bam'),
-        sv_bam_fpn=to('data/r1/trumicount/10xn9k_10c/10xn9k_10c_tagged.bam'),
+        # bam_fpn='/mnt/d/Document/Programming/Python/umiche/umiche/data/r1/trumicount/10xn9k_10c/10xn9k_10c.bam',
+        # sv_bam_fpn='/mnt/d/Document/Programming/Python/umiche/umiche/data/r1/trumicount/10xn9k_10c/10xn9k_10c_tagged.bam',
+
+        bam_fpn='/mnt/d/Document/Programming/Python/umiche/umiche/data/r1/umitools/umitools.test.RNA-seq.sorted.bam',
+        sv_bam_fpn='/mnt/d/Document/Programming/Python/umiche/umiche/data/r1/umitools/umitools.test.RNA-seq.sorted.tagged.bam',
+
         verbose=True,
     )
-    print(p.add_from_header())
+    print(p.add_from_header(
+        tag_info={
+            # 'CB': {'idx': -2, 'val_type': 'Z'},
+            'MB': {'idx': -1, 'val_type': 'Z'},
+        }
+    ))
