c3a0ff8c8dc15effa706e91379309d5a54e19223
Jianfeng Sun <jianfeng.sunmt@gmail.com>
2025-09-05 21:23:32 +0100

    Create SeqtechIllustration.py

---
+++ umiche/plot/scenario/seqtech/SeqtechIllustration.py
@@ -0,0 +1,98 @@
+params = {
+    'vasaseq': {
+        'umi': {'len': 10},
+        'barcode': {'len': 10},
+    },
+    'pipseq': {
+        'barcode1': {'len': 8},
+        'spacer1': {'len': 7, 'seq': 'ATGCATC'},
+        'barcode2': {'len': 8},
+        'spacer2': {'len': 7, 'seq': 'CCTCGAG'},
+        'barcode3': {'len': 8},
+        'umi': {'len': 12},
+        'dT(s)': {'len': 19, 'seq': 'TTTTTTTTTTTTTTTTTTT'},
+    },
+    'scifiseq': {
+        'umi': {'len': 8},
+        'barcode': {'len': 13},
+        'dT(s)': {'len': 30, 'seq': 'TTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'},
+    },
+    'scrbseq': {
+        'umi': {'len': 10},
+        'barcode': {'len': 10},
+    },
+    'shareseq': {
+        'umi': {'len': 10},
+        'barcode': {'len': 10},
+    },
+    'snareseq': {
+        'umi': {'len': 10},
+        'barcode': {'len': 10},
+    },
+    'splitseq': {
+        'umi': {'len': 10},
+        'barcode': {'len': 10},
+    },
+    'strtseqc1': {
+        'umi': {'len': 10},
+        'barcode': {'len': 10},
+    },
+    'strtseq2i': {
+        'umi': {'len': 10},
+        'barcode': {'len': 10},
+    },
+    'quartzseq2': {
+        'barcode': {'len': 15},
+        'umi': {'len': 8},
+        'dT(s)': {'len': 24, 'seq': 'TTTTTTTTTTTTTTTTTTTTTTTT'},
+    },
+    'petipseq': {
+        'umi': {'len': 7},
+        'barcode': {'len': 7},
+        'tail': {'len': 8, 'seq': 'GGTCCTTG'},
+    },
+    'marsseq2': {
+        'barcode': {'len': 6},
+        'umi': {'len': 4},
+        'dT(s)': {'len': 20, 'seq': 'TTTTTTTTTTTTTTTTTTTT'},
+    },
+    'pairedseq': {
+        'primer': {'len': 22, 'seq': 'TCTAGCCTTCTCGTGTGCAGAC'},
+        'umi': {'len': 10},
+        'barcode': {'len': 7},
+    },
+    'issaacseq': {
+        'umi': {'len': 10},
+        'dT(s)': {'len': 30,  'seq': 'TTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'},
+    },
+    'indrop': {
+        'barcode': {'len': 8},
+        'umi': {'len': 6},
+        'dT(s)': {'len': 18, 'seq': 'TTTTTTTTTTTTTTTTTT'},
+    },
+    'dropseq': {
+        'barcode': {'len': 12},
+        'umi': {'len': 8},
+        'dT(s)': {'len': 30},
+    },
+    '10xv2': {
+        'barcode': {'len': 16},
+        'umi': {'len': 10},
+        'dT(s)': {'len': 30},
+    },
+    '10xv3': {
+        'barcode': {'len': 16},
+        'umi': {'len': 12},
+        'dT(s)': {'len': 30},
+    },
+    'celseq2': {
+        'umi': {'len': 6},
+        'barcode': {'len': 6},
+        'dT(s)': {'len': 24},
+    },
+    'flashseq': {
+        'umi': {'len': 8},
+        'spacer': {'len': 5, 'seq': 'CTAAC'},
+        'rb(G)s': {'len': 3, 'seq': 'GGG'},
+    },
+}
